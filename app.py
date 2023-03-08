231b1b009c8838f4ec48f8addda7a9096d25a152
afreenpoly <afreendeby@gmail.com>
2023-03-08 22:52:54 +0530

    replaced with templates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 
 def hi():
-  return "hello world"
+  return render_template("home.html")
 
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
