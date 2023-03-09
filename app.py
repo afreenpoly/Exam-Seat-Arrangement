3a01fa32bf4c2ffe4c9ab0ac8e71e7b39cac578c
afreenpoly <afreendeby@gmail.com>
2023-03-09 22:20:14 +0530

    tried submitting and storing data

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request
+import json
 app=Flask(__name__)
 
-@app.route("/")
+stufiles=open("studetails.txt","r")
+list=json.load(stufiles)
 
+@app.route("/")
 def hi():
   return render_template("home.html")
 
+
+
+@app.route('/submit', methods=["POST"])
+def home1():
+    dict=request.form
+    list.append(dict)
+    stufile=open("studetails.txt","w")
+    json.dump(list,stufile)
+    stufile.close()
+    return list
+
+
+  
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
-  
+
