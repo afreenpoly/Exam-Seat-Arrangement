4a711c1281efdeede483c11a066f1d8cb4a2bfe7
afreenpoly <afreendeby@gmail.com>
2023-03-16 21:26:46 +0530

    project logic completed

--- app.py
+++ app.py
@@ -1,68 +1,114 @@
 from math import ceil
-from flask import Flask,render_template,request
-import json,pandas as pd
-app=Flask(__name__)
+from flask import Flask, render_template, request
+import json
+import pandas as pd
+app = Flask(__name__)
 
-stufiles=open("studetails.txt","r")
-stulist=json.load(stufiles)
+stufiles = open("studetails.txt", "r")
+stulist = json.load(stufiles)
 
 listy = []
 
+
 @app.route("/")
 def hi():
-  return render_template("home.html")
-
+    return render_template("home.html")
 
 
 @app.route('/submit', methods=["POST"])
 def home1():
-    dict=request.form
+    dict = request.form
     stulist.append(dict)
-    stufile=open("studetails.txt","w")
-    json.dump(stulist,stufile)
+    stufile = open("studetails.txt", "w")
+    json.dump(stulist, stufile)
     stufile.close()
     return stulist
+
 
 @app.route('/data', methods=["GET"])
 def data():
     studata = open("Test.csv", "r")
-    exams=[]
+    exams = []
     while True:
         line = studata.readline()
         if not line:
             break
-        ligma = line.replace('\n','').split(',')
+        ligma = line.replace('\n', '').split(',')
         if not ligma[2] in exams:
             exams.append(ligma[2])
-            dict={'name':ligma[2],'ro':[]}
+            dict = {'name': ligma[2], 'ro': []}
             listy.append(dict)
     studata.seek(0)
     while True:
         line = studata.readline()
         if not line:
             break
-        ligma = line.replace('\n','').split(',')
+        ligma = line.replace('\n', '').split(',')
         for d in listy:
             if d['name'] == ligma[2]:
                 d['ro'].append(ligma[0])
     return listy
 
+
 @app.route('/seating', methods=["GET"])
 def seating():
-  k=0;
+  k = 0
   for i in stulist:
-    a=ceil(i["column"]/2)*i["rows"]
-    b=(i["column"]*i["rows"])-a
-    while k<a:
-      for j in listy:
-        if len(j["ro"])!=0:
-          i["a"].append(j["ro"][0])
-          del j["ro"][0]
-          
-          
-          
+      if len(listy)==0:
+        break
+      a = ceil(int(i["column"])/2)*int(i["rows"])
+      b = (int(i["column"])*int(i["rows"]))-a
+      print(listy)
+      print("\n")
+      firstitem = listy[0]
+      listy.pop(0)
+      print(listy)
+      print("\n")
+      print(stulist)
+      print("\n")
+      for j in range(0, a):
+        if len(firstitem["ro"]) == 0:
+          firstitem = listy[0]
+          listy.pop(0)
+          print(listy)
+          print("\n")
+          print(stulist)
+          print("\n")
+        i["a"].append(firstitem["ro"][0])
+        firstitem["ro"].pop(0)
+        print(listy)
+        print("\n")
+        print(stulist)
+        print("\n")
+      if len(firstitem["ro"]) != 0:
+        listy.append(firstitem)
+        firstitem = listy[0]
+        listy.pop(0)
+        print(listy)
+        print("\n")
+        print(stulist)
+        print("\n")
+      for k in range(0, b):
+        if len(firstitem["ro"]) == 0:
+          firstitem = listy[0]
+          listy.pop(0)
+          print(listy)
+          print("\n")
+          print(stulist)
+          print("\n")
+        i["b"].append(firstitem["ro"][0])
+        firstitem["ro"].pop(0)
+        print(listy)
+        print("\n")
+        print(stulist)
+        print("\n")
+      if len(firstitem["ro"]) != 0:
+        listy.append(firstitem)
+        print(listy)
+        print("\n")
+        print(stulist)
+        print("\n")
+  return list(stulist)
 
-  
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
